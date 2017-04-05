86c3f5af2c24cd5d19e629fcb12c0c8f11b49a89
allenliao <allenliao@gmail.com>
2017-04-05 10:14:10 +0800

    grab request data

--- controllers/loginController.go
+++ controllers/loginController.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"log"
 
 	"github.com/astaxie/beego"
 )
@@ -10,28 +13,36 @@ type LoginController struct {
 	beego.Controller
 }
 
+type APIGameLoginInput struct {
+	Token string
+}
+
 func (loginController *LoginController) Post() {
 	//Hard code 一份 JSON 去BY PASS 驗證吧
 
-	responseSuccJsonStr := fmt.Sprintf(`
+	var inputObj APIGameLoginInput
+	json.Unmarshal(loginController.Ctx.Input.RequestBody, &inputObj) //把JSON值塞進Object去
+	var responseJsonStr string
+	log.Printf("RequestBody: %v ", string(loginController.Ctx.Input.RequestBody))
+	log.Printf("inputObj.Token: %v ", inputObj.Token)
+	if inputObj.Token == "aaaaaaaa" {
+
+		responseJsonStr = fmt.Sprintf(`
 		{
 			"Statuscode":"%v",
 			"Membercode":"%v",
 			"Balance":%v
 		}
 		`, "000000", "Allen", 80000)
-	loginController.Ctx.Output.Body([]byte(responseSuccJsonStr))
-
-	/*
-			responseFailJsonStr := fmt.Sprintf(`
+	} else {
+		responseJsonStr = fmt.Sprintf(`
 			{
 				"Statuscode":"%v",
 				"Msg":"%v"
 			}
-			`, "000001", "session timeout")
+			`, "000001", "token expired")
+	}
 
-			loginController.Ctx.Output.Body([]byte(responseFailJsonStr))
+	loginController.Ctx.Output.Body([]byte(responseJsonStr))
 
-		loginController.Ctx.Output.Body([]byte(responseFailJsonStr))
-	*/
 }
